8028229855be56eb665b5248e21a4963f08fedec
adi delibasic <adi.delibasic@gmail.com>
2021-04-08 22:52:11 +0200

    refactor

--- functions.php
+++ functions.php
@@ -15,8 +15,7 @@ add_action('wp_enqueue_scripts', function () {
 // Defer or async JS
 
 add_filter('script_loader_tag', 'add_defer_tags_to_scripts');
-function add_defer_tags_to_scripts($tag)
-{
+function add_defer_tags_to_scripts($tag) {
   # List scripts to add attributes to
   $scripts_to_defer = array('main.bundle.js', 'script_b');
   $scripts_to_async = array('script_c', 'script_d');
@@ -27,7 +26,7 @@ function add_defer_tags_to_scripts($tag)
       return str_replace(' src', ' defer="defer" src', $tag);
   }
 
-  # add the async tags to scripts_to_async array
+  # OR add the async tags to scripts_to_async array
   // foreach ($scripts_to_async as $current_script) {
   //   if (true == strpos($tag, $current_script))
   //     return str_replace(' src', ' async="async" src', $tag);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   purge: ["./**/*.php", "./assets/src/**/*.js"],
 
   fontFamily: {
+    // Import your font here
     montserrat: ["Montserrat", "sans-serif"],
   },
 };
